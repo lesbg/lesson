7a5ac0ca6a8e6d2e9b43c9e2606f17a23f3b4457
Jonathan Dieter <jdieter@lesbg.com>
2016-06-28 13:39:18 +0300

    Whitespace cleanup
    Signed-Off: Jonathan Dieter <jdieter@lesbg.com>

--- admin/user/modify_action.php
+++ admin/user/modify_action.php
@@ -14,149 +14,149 @@ $fullname = dbfuncInt2String($_GET['keyname']) . " (" . $uname . ")";
 
 /* Check whether user is authorized to modify users */
 if ($is_admin) {
-	
-	/* Modify user */
-	$query = "UPDATE user SET FirstName = '{$_POST['fname']}', " .
-			 "Surname = '{$_POST['sname']}', " .
-			 "Gender = '{$_POST['gender']}', " .
-			 "PhoneNumber = '{$_POST['phone']}', " . "DOB = {$_POST['DOB']}, " .
-			 "Permissions = {$_POST['perms']}, " . "Title = {$_POST['title']}, " .
-			 "DateType = {$_POST['datetype']}, " .
-			 "DateSeparator = {$_POST['datesep']}, " .
-			 "DepartmentIndex = {$_POST['department']}";
-	if (isset($_POST['password']) && $_POST['password'] != "") {
-		$phash = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost' => "15"]);
-		$query .= ", Password = '$phash'";
-	}
-	if (isset($_POST['password2']) && $_POST['password2'] != "") {
-		$phash = password_hash($_POST['password2'], PASSWORD_DEFAULT, ['cost' => "15"]);
-		$query .= ", Password2 = '$phash'";
-	}
-	$query .= " WHERE username = '$uname'";
-	$aRes = & $db->query($query);
-	if (DB::isError($aRes))
-		die($aRes->getDebugInfo()); // Check for errors in query
-	
-	if(!isset($_POST['show_family']) || $_POST['show_family'] != '1') {	
-		/* Remove any family codes we've been removed from */
-		$query = "SELECT FamilyListIndex, FamilyCode FROM familylist WHERE Username='$uname'";
-		$aRes = & $db->query($query);
-		if (DB::isError($aRes))
-			die($aRes->getDebugInfo()); // Check for errors in query
-		while ( $arow = & $aRes->fetchRow(DB_FETCHMODE_ASSOC) ) {
-			$found = False;
-			foreach($_POST['fcode'] as $val) {
-				if($aRow['FamilyCode'] == $val[0])
-					$found = True;
-			}
-			if(!$found) {
-				$query = "DELETE FROM familylist WHERE FamilyListIndex={$arow['FamilyListIndex']}";
-				$bRes = & $db->query($query);
-				if (DB::isError($bRes))
-					die($bRes->getDebugInfo()); // Check for errors in query
-			}
-		}
-		
-		/* Add any family codes we've been added to */
-		foreach($_POST['fcode'] as $val) {
-			$fcode = $val[0];
-			$guardian = $val[1];
-			$query = "SELECT FamilyListIndex, FamilyCode FROM familylist WHERE Username='$uname' AND FamilyCode='$fcode'";
-			$aRes = & $db->query($query);
-			if (DB::isError($aRes))
-				die($aRes->getDebugInfo()); // Check for errors in query
-			if ($aRes->numRows() == 0) {
-				$query = "INSERT INTO familylist (Username, FamilyCode, Guardian) VALUES ('$uname', '$fcode', $guardian)";
-				$aRes = & $db->query($query);
-				if (DB::isError($aRes))
-					die($aRes->getDebugInfo()); // Check for errors in query
-			} else {
-				$query = "UPDATE familylist SET Guardian=$guardian WHERE Username='$uname' AND FamilyCode='$fcode'";
-				$aRes = & $db->query($query);
-				if (DB::isError($aRes))
-					die($aRes->getDebugInfo()); // Check for errors in query
-			}
-		}
-	} else {
-		if(!isset($_SESSION['post_family'])) {
-			$_SESSION['post_family'] = array();
-		}
-		if(!isset($_SESSION['post_family']['uname'])) {
-			$_SESSION['post_family']['uname'] = array();
-		}
-		if($_POST['new_user_type'] == 'f' ||$_POST['new_user_type'] == 'm') {
-			$guardian = 1;
-		} else {
-			$guardian = 0;
-		}
-		$_SESSION['post_family']['uname'][] = array($_POST['uname'], $guardian);
-	}
-	
-	/* Remove any groups we've been removed from */
-	$query = "SELECT GroupMemberIndex, GroupID FROM groupmem WHERE Member='$uname'";
-	$aRes = & $db->query($query);
-	if (DB::isError($aRes))
-		die($aRes->getDebugInfo()); // Check for errors in query
-	while ( $arow = & $aRes->fetchRow(DB_FETCHMODE_ASSOC) ) {
-		if(!in_array($arow['GroupID'], $_POST['groups'])) {
-			$query = "DELETE FROM groupmem WHERE GroupMemberIndex={$arow['GroupMemberIndex']}";
-			$bRes = & $db->query($query);
-			if (DB::isError($bRes))
-				die($bRes->getDebugInfo()); // Check for errors in query
-			gen_group_members($arow['GroupID']);	
-		}
-	}
-	
-	/* Add any groups we've been added to */
-	foreach($_POST['groups'] as $group) {
-		$query = "SELECT GroupMemberIndex, GroupID FROM groupmem WHERE Member='$uname' AND GroupID='$group'";
-		$aRes = & $db->query($query);
-		if (DB::isError($aRes))
-			die($aRes->getDebugInfo()); // Check for errors in query
-		if ($aRes->numRows() == 0) {
-			$query = "INSERT INTO groupmem (Member, GroupID) VALUES ('$uname', '$group')";
-			$aRes = & $db->query($query);
-			if (DB::isError($aRes))
-				die($aRes->getDebugInfo()); // Check for errors in query
-		}
-		gen_group_members($group);
-	}
-	
-	/* Remove any phone numbers we've lost */
-	foreach($_POST['phone_remove'] as $phone_index) {
-		$query = "DELETE FROM phone WHERE PhoneIndex='$phone_index'";
-		$aRes = & $db->query($query);
-		if (DB::isError($aRes))
-			die($aRes->getDebugInfo()); // Check for errors in query
-	}
-	
-	/* Add any new phone numbers */
-	$count = 0;
-	foreach($_POST['phone'] as $phone) {
-		$count += 1;
-		if($phone[3] == "") {
-			$comment = "NULL";
-		} else {
-			$comment = "'${phone[3]}'";
-		}
-		if($phone[0] < 0) {
-			$query =	"INSERT INTO phone (SortOrder, Number, Username, Type, Comment) " .
-						"           VALUES ($count, '${phone[1]}', '$uname', ${phone[2]}, $comment)";
-		} else {
-			$query =	"UPDATE phone SET SortOrder=$count, Number='${phone[1]}', Username='$uname', " .
-						"                 Type=${phone[2]}, Comment=$comment WHERE PhoneIndex=${phone[0]}";
-		}
-		$aRes = & $db->query($query);
-		if (DB::isError($aRes))
-			die($aRes->getDebugInfo()); // Check for errors in query
-	}
-	log_event($LOG_LEVEL_ADMIN, "admin/user/modify_action.php", $LOG_ADMIN, 
-		"Modified {$_POST['fname']} {$_POST['sname']} ($uname).");
+
+    /* Modify user */
+    $query = "UPDATE user SET FirstName = '{$_POST['fname']}', " .
+             "Surname = '{$_POST['sname']}', " .
+             "Gender = '{$_POST['gender']}', " .
+             "PhoneNumber = '{$_POST['phone']}', " . "DOB = {$_POST['DOB']}, " .
+             "Permissions = {$_POST['perms']}, " . "Title = {$_POST['title']}, " .
+             "DateType = {$_POST['datetype']}, " .
+             "DateSeparator = {$_POST['datesep']}, " .
+             "DepartmentIndex = {$_POST['department']}";
+    if (isset($_POST['password']) && $_POST['password'] != "") {
+        $phash = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost' => "15"]);
+        $query .= ", Password = '$phash'";
+    }
+    if (isset($_POST['password2']) && $_POST['password2'] != "") {
+        $phash = password_hash($_POST['password2'], PASSWORD_DEFAULT, ['cost' => "15"]);
+        $query .= ", Password2 = '$phash'";
+    }
+    $query .= " WHERE username = '$uname'";
+    $aRes = & $db->query($query);
+    if (DB::isError($aRes))
+        die($aRes->getDebugInfo()); // Check for errors in query
+
+    if(!isset($_POST['show_family']) || $_POST['show_family'] != '1') {
+        /* Remove any family codes we've been removed from */
+        $query = "SELECT FamilyListIndex, FamilyCode FROM familylist WHERE Username='$uname'";
+        $aRes = & $db->query($query);
+        if (DB::isError($aRes))
+            die($aRes->getDebugInfo()); // Check for errors in query
+        while ( $arow = & $aRes->fetchRow(DB_FETCHMODE_ASSOC) ) {
+            $found = False;
+            foreach($_POST['fcode'] as $val) {
+                if($aRow['FamilyCode'] == $val[0])
+                    $found = True;
+            }
+            if(!$found) {
+                $query = "DELETE FROM familylist WHERE FamilyListIndex={$arow['FamilyListIndex']}";
+                $bRes = & $db->query($query);
+                if (DB::isError($bRes))
+                    die($bRes->getDebugInfo()); // Check for errors in query
+            }
+        }
+
+        /* Add any family codes we've been added to */
+        foreach($_POST['fcode'] as $val) {
+            $fcode = $val[0];
+            $guardian = $val[1];
+            $query = "SELECT FamilyListIndex, FamilyCode FROM familylist WHERE Username='$uname' AND FamilyCode='$fcode'";
+            $aRes = & $db->query($query);
+            if (DB::isError($aRes))
+                die($aRes->getDebugInfo()); // Check for errors in query
+            if ($aRes->numRows() == 0) {
+                $query = "INSERT INTO familylist (Username, FamilyCode, Guardian) VALUES ('$uname', '$fcode', $guardian)";
+                $aRes = & $db->query($query);
+                if (DB::isError($aRes))
+                    die($aRes->getDebugInfo()); // Check for errors in query
+            } else {
+                $query = "UPDATE familylist SET Guardian=$guardian WHERE Username='$uname' AND FamilyCode='$fcode'";
+                $aRes = & $db->query($query);
+                if (DB::isError($aRes))
+                    die($aRes->getDebugInfo()); // Check for errors in query
+            }
+        }
+    } else {
+        if(!isset($_SESSION['post_family'])) {
+            $_SESSION['post_family'] = array();
+        }
+        if(!isset($_SESSION['post_family']['uname'])) {
+            $_SESSION['post_family']['uname'] = array();
+        }
+        if($_POST['new_user_type'] == 'f' ||$_POST['new_user_type'] == 'm') {
+            $guardian = 1;
+        } else {
+            $guardian = 0;
+        }
+        $_SESSION['post_family']['uname'][] = array($_POST['uname'], $guardian);
+    }
+
+    /* Remove any groups we've been removed from */
+    $query = "SELECT GroupMemberIndex, GroupID FROM groupmem WHERE Member='$uname'";
+    $aRes = & $db->query($query);
+    if (DB::isError($aRes))
+        die($aRes->getDebugInfo()); // Check for errors in query
+    while ( $arow = & $aRes->fetchRow(DB_FETCHMODE_ASSOC) ) {
+        if(!in_array($arow['GroupID'], $_POST['groups'])) {
+            $query = "DELETE FROM groupmem WHERE GroupMemberIndex={$arow['GroupMemberIndex']}";
+            $bRes = & $db->query($query);
+            if (DB::isError($bRes))
+                die($bRes->getDebugInfo()); // Check for errors in query
+            gen_group_members($arow['GroupID']);
+        }
+    }
+
+    /* Add any groups we've been added to */
+    foreach($_POST['groups'] as $group) {
+        $query = "SELECT GroupMemberIndex, GroupID FROM groupmem WHERE Member='$uname' AND GroupID='$group'";
+        $aRes = & $db->query($query);
+        if (DB::isError($aRes))
+            die($aRes->getDebugInfo()); // Check for errors in query
+        if ($aRes->numRows() == 0) {
+            $query = "INSERT INTO groupmem (Member, GroupID) VALUES ('$uname', '$group')";
+            $aRes = & $db->query($query);
+            if (DB::isError($aRes))
+                die($aRes->getDebugInfo()); // Check for errors in query
+        }
+        gen_group_members($group);
+    }
+
+    /* Remove any phone numbers we've lost */
+    foreach($_POST['phone_remove'] as $phone_index) {
+        $query = "DELETE FROM phone WHERE PhoneIndex='$phone_index'";
+        $aRes = & $db->query($query);
+        if (DB::isError($aRes))
+            die($aRes->getDebugInfo()); // Check for errors in query
+    }
+
+    /* Add any new phone numbers */
+    $count = 0;
+    foreach($_POST['phone'] as $phone) {
+        $count += 1;
+        if($phone[3] == "") {
+            $comment = "NULL";
+        } else {
+            $comment = "'${phone[3]}'";
+        }
+        if($phone[0] < 0) {
+            $query =    "INSERT INTO phone (SortOrder, Number, Username, Type, Comment) " .
+                        "           VALUES ($count, '${phone[1]}', '$uname', ${phone[2]}, $comment)";
+        } else {
+            $query =    "UPDATE phone SET SortOrder=$count, Number='${phone[1]}', Username='$uname', " .
+                        "                 Type=${phone[2]}, Comment=$comment WHERE PhoneIndex=${phone[0]}";
+        }
+        $aRes = & $db->query($query);
+        if (DB::isError($aRes))
+            die($aRes->getDebugInfo()); // Check for errors in query
+    }
+    log_event($LOG_LEVEL_ADMIN, "admin/user/modify_action.php", $LOG_ADMIN,
+        "Modified {$_POST['fname']} {$_POST['sname']} ($uname).");
 } else { // User isn't authorized to view or change users.
-	/* Log unauthorized access attempt */
-	log_event($LOG_LEVEL_ERROR, "admin/user/modify_action.php", 
-			$LOG_DENIED_ACCESS, "Attempted to modify user $fullname.");
-	echo "</p>\n      <p>You do not have permission to modify this user.</p>\n      <p>";
-	$error = true;
+    /* Log unauthorized access attempt */
+    log_event($LOG_LEVEL_ERROR, "admin/user/modify_action.php",
+            $LOG_DENIED_ACCESS, "Attempted to modify user $fullname.");
+    echo "</p>\n      <p>You do not have permission to modify this user.</p>\n      <p>";
+    $error = true;
 }
 ?>
