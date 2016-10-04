801f01c06e221d6e3c7b7273402e6d27b23ba70f
Jonathan Dieter <jdieter@lesbg.com>
2016-10-04 11:36:17 +0300

    Fix longstanding bug where users can't log in if they're not active students or teachers
    Plus annoying whitespace fixes
    
    Signed-off-by: Jonathan Dieter <jdieter@lesbg.com>

--- core/settermandyear.php
+++ core/settermandyear.php
@@ -7,170 +7,171 @@
  * ***************************************************************
  */
 if (isset($_GET['year'])) { // Check whether we're looking at a different year
-	$yearindex = safe(dbfuncInt2String($_GET['year']));
-	if($yearindex != $_SESSION['yearindex'])
-		$termindex = NULL;
-	$_SESSION['yearindex'] = $yearindex;
+    $yearindex = safe(dbfuncInt2String($_GET['year']));
+    if($yearindex != $_SESSION['yearindex'])
+        $termindex = NULL;
+    $_SESSION['yearindex'] = $yearindex;
 }
 
 if (isset($_GET['dept'])) { // Check whether we're looking at a different term
-	$depindex = safe(dbfuncInt2String($_GET['dept']));
+    $depindex = safe(dbfuncInt2String($_GET['dept']));
 }
 if (isset($_GET['term'])) { // Check whether we're looking at a different term
-	$termindex = safe(dbfuncInt2String($_GET['term']));
+    $termindex = safe(dbfuncInt2String($_GET['term']));
 }
 
 if (! isset($admin_page))
-	$admin_page = false;
+    $admin_page = false;
 if (! isset($main_page))
-	$main_page = false;
+    $main_page = false;
 
 if (! isset($showalldeps)) {
-	if ($is_admin and ! $main_page) {
-		$showalldeps = true;
-	} else {
-		$showalldeps = false;
-	}
+    if ($is_admin and ! $main_page) {
+        $showalldeps = true;
+    } else {
+        $showalldeps = false;
+    }
 }
 
 $query = "SELECT YearIndex FROM currentinfo ORDER BY InputDate DESC LIMIT 1";
 $tty_res = &  $db->query($query);
 if (DB::isError($tty_res))
-	die($tty_res->getDebugInfo()); // Check for errors in query
+    die($tty_res->getDebugInfo()); // Check for errors in query
 if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-	$currentyear = $tty_row['YearIndex'];
+    $currentyear = $tty_row['YearIndex'];
 }
 
 if (! isset($depindex)) {
-	/* If department hasn't been set, choose default department for user */
-	$query = "SELECT DepartmentIndex FROM user " .
-			 "WHERE Username = '$username' " . "AND DepartmentIndex IS NOT NULL ";
-	$tty_res = &  $db->query($query);
-	if (DB::isError($tty_res))
-		die($tty_res->getDebugInfo()); // Check for errors in query
-	if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-		$depindex = $tty_row['DepartmentIndex'];
-	} else {
-		$depindex = 3;
-	}
+    /* If department hasn't been set, choose default department for user */
+    $query = "SELECT DepartmentIndex FROM user " .
+             "WHERE Username = '$username' " .
+             "AND DepartmentIndex IS NOT NULL ";
+    $tty_res = &  $db->query($query);
+    if (DB::isError($tty_res))
+        die($tty_res->getDebugInfo()); // Check for errors in query
+    if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
+        $depindex = $tty_row['DepartmentIndex'];
+    } else {
+        $depindex = 3;
+    }
 }
 
 /* Verify that user is allowed to choose specified department */
 if (! $showalldeps) {
-	if (! $admin_page) {
-		$query = "(SELECT class.DepartmentIndex FROM class, classterm, classlist " .
-				 " WHERE classlist.Username         = '$username' " .
-				 " AND   classlist.ClassTermIndex   = classterm.ClassTermIndex " .
-				 " AND   classterm.ClassIndex       = class.ClassIndex " .
-				 " AND   class.YearIndex            = $yearindex) " . "UNION " .
-				 "(SELECT class.DepartmentIndex  " .
-				 "         FROM user, support, class, classterm, classlist, currentterm, groupgenmem, groups " .
-				 " WHERE support.WorkerUsername   = '$username' " .
-				 " AND   user.Username            = support.WorkerUsername " .
-				 " AND   groupgenmem.Username     = user.Username " .
-				 " AND   groups.GroupID           = groupgenmem.GroupID " .
-				 " AND   groups.GroupTypeID       = 'supportteacher' " .
-				 " AND   support.StudentUsername  = classlist.Username " .
-				 " AND   classlist.ClassTermIndex = classterm.ClassTermIndex " .
-				 " AND   classterm.TermIndex      = currentterm.TermIndex " .
-				 " AND   classterm.ClassIndex     = class.ClassIndex " .
-				 " AND   class.YearIndex          = $yearindex" .
-				 " GROUP BY class.DepartmentIndex) " . "UNION " .
-				 "(SELECT subject.DepartmentIndex FROM subject, subjectteacher " .
-				 " WHERE subjectteacher.Username = '$username' " .
-				 " AND   subject.SubjectIndex    = subjectteacher.SubjectIndex " .
-				 " AND   subject.YearIndex       = $yearindex) " . "UNION ";
-		if (! $main_page) {
-			$query .= "(SELECT DepartmentIndex FROM hod " .
-					 " WHERE hod.Username = '$username') " . "UNION " .
-					 "(SELECT department.DepartmentIndex FROM department, principal " .
-					 " WHERE principal.Username='$username') " . "UNION " .
-					 "(SELECT department.DepartmentIndex FROM department, counselorlist " .
-					 " WHERE counselorlist.Username='$username') " . "UNION ";
-		}
-		$query .= "(SELECT DepartmentIndex FROM user " .
-				 " WHERE Username = '$username' " .
-				 " AND DepartmentIndex IS NOT NULL) " .
-				 "ORDER BY DepartmentIndex";
-	} else {
-		$query = "(SELECT DepartmentIndex FROM hod " .
-				 " WHERE hod.Username = '$username') " . "UNION " .
-				 "(SELECT department.DepartmentIndex FROM department, principal " .
-				 " WHERE principal.Username='$username') " . "UNION " .
-				 "(SELECT department.DepartmentIndex FROM department, counselorlist " .
-				 " WHERE counselorlist.Username='$username') " .
-				 "ORDER BY DepartmentIndex";
-	}
+    if (! $admin_page) {
+        $query = "(SELECT class.DepartmentIndex FROM class, classterm, classlist " .
+                 " WHERE classlist.Username         = '$username' " .
+                 " AND   classlist.ClassTermIndex   = classterm.ClassTermIndex " .
+                 " AND   classterm.ClassIndex       = class.ClassIndex " .
+                 " AND   class.YearIndex            = $yearindex) " . "UNION " .
+                 "(SELECT class.DepartmentIndex  " .
+                 "         FROM user, support, class, classterm, classlist, currentterm, groupgenmem, groups " .
+                 " WHERE support.WorkerUsername   = '$username' " .
+                 " AND   user.Username            = support.WorkerUsername " .
+                 " AND   groupgenmem.Username     = user.Username " .
+                 " AND   groups.GroupID           = groupgenmem.GroupID " .
+                 " AND   groups.GroupTypeID       = 'supportteacher' " .
+                 " AND   support.StudentUsername  = classlist.Username " .
+                 " AND   classlist.ClassTermIndex = classterm.ClassTermIndex " .
+                 " AND   classterm.TermIndex      = currentterm.TermIndex " .
+                 " AND   classterm.ClassIndex     = class.ClassIndex " .
+                 " AND   class.YearIndex          = $yearindex" .
+                 " GROUP BY class.DepartmentIndex) " . "UNION " .
+                 "(SELECT subject.DepartmentIndex FROM subject, subjectteacher " .
+                 " WHERE subjectteacher.Username = '$username' " .
+                 " AND   subject.SubjectIndex    = subjectteacher.SubjectIndex " .
+                 " AND   subject.YearIndex       = $yearindex) " . "UNION ";
+        if (! $main_page) {
+            $query .= "(SELECT DepartmentIndex FROM hod " .
+                     " WHERE hod.Username = '$username') " . "UNION " .
+                     "(SELECT department.DepartmentIndex FROM department, principal " .
+                     " WHERE principal.Username='$username') " . "UNION " .
+                     "(SELECT department.DepartmentIndex FROM department, counselorlist " .
+                     " WHERE counselorlist.Username='$username') " . "UNION ";
+        }
+        $query .= "(SELECT DepartmentIndex FROM user " .
+                 " WHERE Username = '$username' " .
+                 " AND DepartmentIndex IS NOT NULL) " .
+                 "ORDER BY DepartmentIndex";
+    } else {
+        $query = "(SELECT DepartmentIndex FROM hod " .
+                 " WHERE hod.Username = '$username') " . "UNION " .
+                 "(SELECT department.DepartmentIndex FROM department, principal " .
+                 " WHERE principal.Username='$username') " . "UNION " .
+                 "(SELECT department.DepartmentIndex FROM department, counselorlist " .
+                 " WHERE counselorlist.Username='$username') " .
+                 "ORDER BY DepartmentIndex";
+    }
 } else {
-	$query = "SELECT DepartmentIndex FROM department " .
-			 "ORDER BY DepartmentIndex ";
+    $query = "SELECT DepartmentIndex FROM department " .
+             "ORDER BY DepartmentIndex ";
 }
 $tty_res = &  $db->query($query);
 if (DB::isError($tty_res))
-	die($tty_res->getDebugInfo()); // Check for errors in query
+    die($tty_res->getDebugInfo()); // Check for errors in query
 if ($tty_res->numRows() > 0) {
-	if (isset($depindex)) {
-		while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) {
-			if (! isset($final))
-				$final = $tty_row['DepartmentIndex'];
-			if ($depindex == $tty_row['DepartmentIndex'])
-				$final = $depindex;
-		}
-		$depindex = $final;
-	} else {
-		$tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC);
-		$depindex = $tty_row['DepartmentIndex'];
-	}
+    if (isset($depindex)) {
+        while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) {
+            if (! isset($final))
+                $final = $tty_row['DepartmentIndex'];
+            if ($depindex == $tty_row['DepartmentIndex'])
+                $final = $depindex;
+        }
+        $depindex = $final;
+    } else {
+        $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC);
+        $depindex = $tty_row['DepartmentIndex'];
+    }
 } else {
-	$depindex = NULL;
+    $depindex = 3;
 }
 /* Store chosen department */
 $_SESSION['depindex'] = $depindex;
 
 $query = "SELECT TermIndex FROM currentterm " .
-		 "WHERE DepartmentIndex=$depindex ";
+         "WHERE DepartmentIndex=$depindex ";
 $tty_res = &  $db->query($query);
 if (DB::isError($tty_res))
-	die($tty_res->getDebugInfo()); // Check for errors in query
+    die($tty_res->getDebugInfo()); // Check for errors in query
 if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-	$currentterm = $tty_row['TermIndex'];
+    $currentterm = $tty_row['TermIndex'];
 } else {
-	$currentterm = NULL;
+    $currentterm = 1;
 }
 
 if (isset($termindex)) {
-	$query =	"SELECT TermIndex FROM term " . 
-				"WHERE DepartmentIndex=$depindex " .
-				"AND   TermIndex=$termindex";
-	$tty_res = &  $db->query($query);
-	if (DB::isError($tty_res))
-		die($tty_res->getDebugInfo()); // Check for errors in query
-	if($tty_res->numRows() == 0)
-		$termindex = NULL;
+    $query =    "SELECT TermIndex FROM term " .
+                "WHERE DepartmentIndex=$depindex " .
+                "AND   TermIndex=$termindex";
+    $tty_res = &  $db->query($query);
+    if (DB::isError($tty_res))
+        die($tty_res->getDebugInfo()); // Check for errors in query
+    if($tty_res->numRows() == 0)
+        $termindex = 1;
 }
 
 if(!isset($termindex) || is_null($termindex)) {
-	$termindex = $currentterm;	
-	
-	$query = "SELECT YearIndex FROM currentinfo ORDER BY InputDate DESC LIMIT 1";
-	$tty_res =& $db->query($query);
-	if (DB::isError($tty_res))
-		die($tty_res->getDebugInfo()); // Check for errors in query
+    $termindex = $currentterm;
 
-	if ($tty_row =& $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-		$current_year_index = $tty_row['YearIndex'];
-		if($current_year_index != $yearindex) {
-			$query =	"SELECT TermIndex FROM term " . 
-						"WHERE DepartmentIndex=$depindex " .
-						"AND TermNumber=1";
-			$tty_res =& $db->query($query);
-			if (DB::isError($tty_res))
-				die($tty_res->getDebugInfo()); // Check for errors in query
-			if ($tty_row =& $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-				$termindex = $tty_row['TermIndex'];
-			}
-		}		
-	}
+    $query = "SELECT YearIndex FROM currentinfo ORDER BY InputDate DESC LIMIT 1";
+    $tty_res =& $db->query($query);
+    if (DB::isError($tty_res))
+        die($tty_res->getDebugInfo()); // Check for errors in query
+
+    if ($tty_row =& $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
+        $current_year_index = $tty_row['YearIndex'];
+        if($current_year_index != $yearindex) {
+            $query =    "SELECT TermIndex FROM term " .
+                        "WHERE DepartmentIndex=$depindex " .
+                        "AND TermNumber=1";
+            $tty_res =& $db->query($query);
+            if (DB::isError($tty_res))
+                die($tty_res->getDebugInfo()); // Check for errors in query
+            if ($tty_row =& $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $termindex = $tty_row['TermIndex'];
+            }
+        }
+    }
 }
 
 $_SESSION['termindex'] = $termindex;
