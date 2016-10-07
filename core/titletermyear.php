6e26d02c40ffd2856957224d361e84fb5248cf4b
Jonathan Dieter <jdieter@lesbg.com>
2016-10-07 21:26:48 +0300

    Fix small bug where dept was getting sent twice (and stupid whitespace fixes)
    Signed-off-by: Jonathan Dieter <jdieter@lesbg.com>

--- core/titletermyear.php
+++ core/titletermyear.php
@@ -12,187 +12,187 @@ $tty_newyear = NULL;
 $tty_oldterm = NULL;
 $tty_newterm = NULL;
 if (! isset($nochangeyt))
-	$nochangeyt = false;
+    $nochangeyt = false;
 if (! isset($nochangeyear))
-	$nochangeyear = false;
+    $nochangeyear = false;
 if (! isset($nochangeterm))
-	$nochangeterm = false;
+    $nochangeterm = false;
 if (! isset($showterm))
-	$showterm = true;
+    $showterm = true;
 if (! isset($showyear))
-	$showyear = true;
+    $showyear = true;
 if (! isset($showdeps))
-	$showdeps = true;
+    $showdeps = true;
 if (! isset($showalldeps))
-	$showalldeps = false;
+    $showalldeps = false;
 
 $tty_link = "index.php?location=" . $_GET['location'];
 
 foreach ( $_GET as $tty_getkey => $getval ) {
-	if ($tty_getkey != 'year' && $tty_getkey != 'term' &&
-		 $tty_getkey != 'location')
-		$tty_link .= "&amp;$tty_getkey=$getval";
+    if ($tty_getkey != 'year' && $tty_getkey != 'term' &&
+         $tty_getkey != 'dept' && $tty_getkey != 'location')
+        $tty_link .= "&amp;$tty_getkey=$getval";
 }
 if ($showyear) {
-	$tty_res = &   $db->query(
-						"SELECT Year, YearIndex FROM year " . // Run query to get year
-						 "ORDER BY YearNumber");
-	if (DB::isError($tty_res))
-		die($tty_res->getDebugInfo()); // Check for errors in query
-	while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) { // If there is a year, print it
-		if ($tty_row['YearIndex'] == $yearindex) {
-			$tty_yearname = $tty_row['Year'];
-			if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-				$tty_newyear = $tty_row['YearIndex'];
-			}
-			break;
-		} else {
-			$tty_oldyear = $tty_row['YearIndex'];
-		}
-	}
-	
-	if ($nochangeyt == false and $nochangeyear == false) {
-		if (! is_null($tty_oldyear)) {
-			$tty_nlink = $tty_link . "&amp;year=" .
-				 dbfuncString2Int($tty_oldyear);
-			$tty_prevbutton = dbfuncGetButton($tty_nlink, "<", "small", 
-											"prevnext", "Previous school year");
-		} else {
-			$tty_prevbutton = dbfuncGetDisabledButton("<", "small", "prevnext");
-		}
-		if (! is_null($tty_newyear)) {
-			$tty_nlink = $tty_link . "&amp;year=" .
-						 dbfuncString2Int($tty_newyear);
-			$tty_nextbutton = dbfuncGetButton($tty_nlink, ">", "small", 
-											"prevnext", "Next school year");
-		} else {
-			$tty_nextbutton = dbfuncGetDisabledButton(">", "small", "prevnext");
-		}
-		echo "      <h3>$tty_prevbutton $tty_yearname $tty_nextbutton</h3>\n";
-	} else {
-		echo "      <h3>$tty_yearname</h3>\n";
-	}
+    $tty_res = &   $db->query(
+                        "SELECT Year, YearIndex FROM year " . // Run query to get year
+                         "ORDER BY YearNumber");
+    if (DB::isError($tty_res))
+        die($tty_res->getDebugInfo()); // Check for errors in query
+    while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) { // If there is a year, print it
+        if ($tty_row['YearIndex'] == $yearindex) {
+            $tty_yearname = $tty_row['Year'];
+            if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
+                $tty_newyear = $tty_row['YearIndex'];
+            }
+            break;
+        } else {
+            $tty_oldyear = $tty_row['YearIndex'];
+        }
+    }
+
+    if ($nochangeyt == false and $nochangeyear == false) {
+        if (! is_null($tty_oldyear)) {
+            $tty_nlink = $tty_link . "&amp;year=" .
+                 dbfuncString2Int($tty_oldyear);
+            $tty_prevbutton = dbfuncGetButton($tty_nlink, "<", "small",
+                                            "prevnext", "Previous school year");
+        } else {
+            $tty_prevbutton = dbfuncGetDisabledButton("<", "small", "prevnext");
+        }
+        if (! is_null($tty_newyear)) {
+            $tty_nlink = $tty_link . "&amp;year=" .
+                         dbfuncString2Int($tty_newyear);
+            $tty_nextbutton = dbfuncGetButton($tty_nlink, ">", "small",
+                                            "prevnext", "Next school year");
+        } else {
+            $tty_nextbutton = dbfuncGetDisabledButton(">", "small", "prevnext");
+        }
+        echo "      <h3>$tty_prevbutton $tty_yearname $tty_nextbutton</h3>\n";
+    } else {
+        echo "      <h3>$tty_yearname</h3>\n";
+    }
 }
 
 if ($nochangeyt == false and isset($depindex) and ! is_null($depindex) and
-	 $showdeps) {
-	if (! $showalldeps) {
-		if (! $admin_page) {
-			$query = "(SELECT department.Department, class.DepartmentIndex FROM class, classterm, classlist, " .
-			 "        department " .
-			 " WHERE classlist.Username         = '$username' " .
-			 " AND   classlist.ClassTermIndex   = classterm.ClassTermIndex " .
-			 " AND   classterm.TermIndex        = $termindex " .
-			 " AND   classterm.ClassIndex       = class.ClassIndex " .
-			 " AND   class.YearIndex            = $yearindex " .
-			 " AND   department.DepartmentIndex = class.DepartmentIndex) " .
-			 "UNION " . "(SELECT department.Department, class.DepartmentIndex  " .
-			 "         FROM department, user, support, class, classterm, classlist, currentterm, groupgenmem, groups " .
-			 " WHERE support.WorkerUsername   = '$username' " .
-			 " AND   user.Username            = support.WorkerUsername " .
-			 " AND   groupgenmem.Username     = user.Username " .
-			 " AND   groups.GroupID           = groupgenmem.GroupID " .
-			 " AND   groups.GroupTypeID       = 'supportteacher' " .
-			 " AND   support.StudentUsername  = classlist.Username " .
-			 " AND   classlist.ClassTermIndex = classterm.ClassTermIndex " .
-			 " AND   classterm.TermIndex      = currentterm.TermIndex " .
-			 " AND   classterm.ClassIndex     = class.ClassIndex " .
-			 " AND   class.YearIndex          = $yearindex" .
-			 " AND   department.DepartmentIndex = class.DepartmentIndex " .
-			 " GROUP BY class.DepartmentIndex) " . "UNION " .
-			 "(SELECT department.Department, subject.DepartmentIndex FROM subject, subjectteacher, " .
-			 "        department " .
-			 " WHERE subjectteacher.Username = \"$username\" " .
-			 " AND   subject.SubjectIndex    = subjectteacher.SubjectIndex " .
-			 " AND   subject.YearIndex       = $yearindex " .
-			 " AND   department.DepartmentIndex = subject.DepartmentIndex) " .
-	/*						"UNION " .
-							"(SELECT department.Department, DepartmentIndex FROM hod " .
-							" WHERE hod.Username = \"$username\") " .
-							"UNION " .
-							"(SELECT department.DepartmentIndex FROM department, principal " .
-							" WHERE principal.Username=\"$username\") " .
-							"UNION " .
-							"(SELECT department.DepartmentIndex FROM department, counselorlist " .
-							" WHERE counselorlist.Username=\"$username\") " .*/
-							"UNION " .
-			 "(SELECT department.Department, user.DepartmentIndex FROM user, department " .
-			 " WHERE user.Username = \"$username\" " .
-			 " AND department.DepartmentIndex = user.DepartmentIndex) " .
-			 "ORDER BY DepartmentIndex";
-	} else {
-		$query = "(SELECT hod.DepartmentIndex, department.Department FROM " .
-				 "        hod INNER JOIN department USING (DepartmentIndex) " .
-				 " WHERE hod.Username = '$username') " . "UNION " .
-				 "(SELECT department.DepartmentIndex, department.Department FROM " .
-				 "        department, principal " .
-				 " WHERE principal.Username='$username') " . "UNION " .
-				 "(SELECT department.DepartmentIndex, department.Department FROM " .
-				 "        department, counselorlist " .
-				 " WHERE counselorlist.Username='$username') " .
-				 "ORDER BY DepartmentIndex";
-	}
+     $showdeps) {
+    if (! $showalldeps) {
+        if (! $admin_page) {
+            $query = "(SELECT department.Department, class.DepartmentIndex FROM class, classterm, classlist, " .
+             "        department " .
+             " WHERE classlist.Username         = '$username' " .
+             " AND   classlist.ClassTermIndex   = classterm.ClassTermIndex " .
+             " AND   classterm.TermIndex        = $termindex " .
+             " AND   classterm.ClassIndex       = class.ClassIndex " .
+             " AND   class.YearIndex            = $yearindex " .
+             " AND   department.DepartmentIndex = class.DepartmentIndex) " .
+             "UNION " . "(SELECT department.Department, class.DepartmentIndex  " .
+             "         FROM department, user, support, class, classterm, classlist, currentterm, groupgenmem, groups " .
+             " WHERE support.WorkerUsername   = '$username' " .
+             " AND   user.Username            = support.WorkerUsername " .
+             " AND   groupgenmem.Username     = user.Username " .
+             " AND   groups.GroupID           = groupgenmem.GroupID " .
+             " AND   groups.GroupTypeID       = 'supportteacher' " .
+             " AND   support.StudentUsername  = classlist.Username " .
+             " AND   classlist.ClassTermIndex = classterm.ClassTermIndex " .
+             " AND   classterm.TermIndex      = currentterm.TermIndex " .
+             " AND   classterm.ClassIndex     = class.ClassIndex " .
+             " AND   class.YearIndex          = $yearindex" .
+             " AND   department.DepartmentIndex = class.DepartmentIndex " .
+             " GROUP BY class.DepartmentIndex) " . "UNION " .
+             "(SELECT department.Department, subject.DepartmentIndex FROM subject, subjectteacher, " .
+             "        department " .
+             " WHERE subjectteacher.Username = \"$username\" " .
+             " AND   subject.SubjectIndex    = subjectteacher.SubjectIndex " .
+             " AND   subject.YearIndex       = $yearindex " .
+             " AND   department.DepartmentIndex = subject.DepartmentIndex) " .
+    /*                      "UNION " .
+                            "(SELECT department.Department, DepartmentIndex FROM hod " .
+                            " WHERE hod.Username = \"$username\") " .
+                            "UNION " .
+                            "(SELECT department.DepartmentIndex FROM department, principal " .
+                            " WHERE principal.Username=\"$username\") " .
+                            "UNION " .
+                            "(SELECT department.DepartmentIndex FROM department, counselorlist " .
+                            " WHERE counselorlist.Username=\"$username\") " .*/
+                            "UNION " .
+             "(SELECT department.Department, user.DepartmentIndex FROM user, department " .
+             " WHERE user.Username = \"$username\" " .
+             " AND department.DepartmentIndex = user.DepartmentIndex) " .
+             "ORDER BY DepartmentIndex";
+    } else {
+        $query = "(SELECT hod.DepartmentIndex, department.Department FROM " .
+                 "        hod INNER JOIN department USING (DepartmentIndex) " .
+                 " WHERE hod.Username = '$username') " . "UNION " .
+                 "(SELECT department.DepartmentIndex, department.Department FROM " .
+                 "        department, principal " .
+                 " WHERE principal.Username='$username') " . "UNION " .
+                 "(SELECT department.DepartmentIndex, department.Department FROM " .
+                 "        department, counselorlist " .
+                 " WHERE counselorlist.Username='$username') " .
+                 "ORDER BY DepartmentIndex";
+    }
 } else {
-	$query = "SELECT DepartmentIndex, Department FROM department " .
-			 "ORDER BY DepartmentIndex ";
+    $query = "SELECT DepartmentIndex, Department FROM department " .
+             "ORDER BY DepartmentIndex ";
 }
 $tty_res = &  $db->query($query);
 if (DB::isError($tty_res))
-	die($tty_res->getDebugInfo()); // Check for errors in query
+    die($tty_res->getDebugInfo()); // Check for errors in query
 if ($tty_res->numRows() > 1) {
-	echo "      <h3><i>";
-	while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) {
-		if ($tty_row['DepartmentIndex'] == $depindex) {
-			$buttontype = "selected";
-		} else {
-			$buttontype = "prevnext";
-		}
-		$tty_nlink = $tty_link . "&amp;dept=" .
-					 dbfuncString2Int($tty_row['DepartmentIndex']);
-		$tty_button = dbfuncGetButton($tty_nlink, $tty_row['Department'], 
-									"small", $buttontype, 
-									"{$tty_row['Department']} department");
-		echo "$tty_button ";
-	}
-	echo "</i></h3>\n";
+    echo "      <h3><i>";
+    while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) {
+        if ($tty_row['DepartmentIndex'] == $depindex) {
+            $buttontype = "selected";
+        } else {
+            $buttontype = "prevnext";
+        }
+        $tty_nlink = $tty_link . "&amp;dept=" .
+                     dbfuncString2Int($tty_row['DepartmentIndex']);
+        $tty_button = dbfuncGetButton($tty_nlink, $tty_row['Department'],
+                                    "small", $buttontype,
+                                    "{$tty_row['Department']} department");
+        echo "$tty_button ";
+    }
+    echo "</i></h3>\n";
 }
 }
 if (isset($termindex) and ! is_null($termindex) and $showterm) {
 $tty_res = &  $db->query(
-					"SELECT TermName, TermIndex FROM term " . // Run query to get term
-							 "WHERE DepartmentIndex = $depindex " . "ORDER BY TermNumber");
+                    "SELECT TermName, TermIndex FROM term " . // Run query to get term
+                             "WHERE DepartmentIndex = $depindex " . "ORDER BY TermNumber");
 if (DB::isError($tty_res))
-	die($tty_res->getDebugInfo()); // Check for errors in query
+    die($tty_res->getDebugInfo()); // Check for errors in query
 while ( $tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC) ) { // If there is a year, print it
-	if ($tty_row['TermIndex'] == $termindex) {
-		$tty_termname = $tty_row['TermName'];
-		if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
-			$tty_newterm = $tty_row['TermIndex'];
-		}
-		break;
-	} else {
-		$tty_oldterm = $tty_row['TermIndex'];
-	}
+    if ($tty_row['TermIndex'] == $termindex) {
+        $tty_termname = $tty_row['TermName'];
+        if ($tty_row = & $tty_res->fetchRow(DB_FETCHMODE_ASSOC)) {
+            $tty_newterm = $tty_row['TermIndex'];
+        }
+        break;
+    } else {
+        $tty_oldterm = $tty_row['TermIndex'];
+    }
 }
 
 if ($nochangeyt == false and $nochangeterm == false) {
-	if (! is_null($tty_oldterm)) {
-		$tty_nlink = $tty_link . "&amp;term=" . dbfuncString2Int($tty_oldterm);
-		$tty_prevbutton = dbfuncGetButton($tty_nlink, "<", "small", "prevnext", 
-										"Previous term");
-	} else {
-		$tty_prevbutton = dbfuncGetDisabledButton("<", "small", "prevnext");
-	}
-	if (! is_null($tty_newterm)) {
-		$tty_nlink = $tty_link . "&amp;term=" . dbfuncString2Int($tty_newterm);
-		$tty_nextbutton = dbfuncGetButton($tty_nlink, ">", "small", "prevnext", 
-										"Next term");
-	} else {
-		$tty_nextbutton = dbfuncGetDisabledButton(">", "small", "prevnext");
-	}
-	echo "      <h3><i>$tty_prevbutton $tty_termname $tty_nextbutton</i></h3>\n";
+    if (! is_null($tty_oldterm)) {
+        $tty_nlink = $tty_link . "&amp;term=" . dbfuncString2Int($tty_oldterm);
+        $tty_prevbutton = dbfuncGetButton($tty_nlink, "<", "small", "prevnext",
+                                        "Previous term");
+    } else {
+        $tty_prevbutton = dbfuncGetDisabledButton("<", "small", "prevnext");
+    }
+    if (! is_null($tty_newterm)) {
+        $tty_nlink = $tty_link . "&amp;term=" . dbfuncString2Int($tty_newterm);
+        $tty_nextbutton = dbfuncGetButton($tty_nlink, ">", "small", "prevnext",
+                                        "Next term");
+    } else {
+        $tty_nextbutton = dbfuncGetDisabledButton(">", "small", "prevnext");
+    }
+    echo "      <h3><i>$tty_prevbutton $tty_termname $tty_nextbutton</i></h3>\n";
 } else {
-	echo "      <h3><i>$tty_termname</i></h3>\n";
+    echo "      <h3><i>$tty_termname</i></h3>\n";
 }
 }
 ?>
