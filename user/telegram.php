387fa0394cac107fcd7ee440f7debbf4af619a41
Jonathan Dieter <jdieter@lesbg.com>
2016-11-29 15:23:38 +0200

    Update Telegram title and whitespace fixes
    Signed-off-by: Jonathan Dieter <jdieter@lesbg.com>

--- user/telegram.php
+++ user/telegram.php
@@ -7,7 +7,7 @@
  * ***************************************************************
  */
 
-$title = "Send message";
+$title = "Telegram";
 
 include "header.php"; // Show header
 
@@ -17,27 +17,27 @@ $db = & dbfuncConnectMaster(); // Connect to database and store in $db
 $query = "DELETE FROM api_validate WHERE ValidUntil < NOW() ";
 $res = & $db->query($query);
 if (DB::isError($res))
-	die($res->getDebugInfo()); // Check for errors in query
+    die($res->getDebugInfo()); // Check for errors in query
 
 $username=safe($username);
 
 $query = "SELECT Hash FROM api_validate WHERE Username='$username'";
 $res = & $db->query($query);
 if (DB::isError($res))
-	die($res->getDebugInfo()); // Check for errors in query
+    die($res->getDebugInfo()); // Check for errors in query
 
 if ($row = & $res->fetchRow(DB_FETCHMODE_ASSOC)) {
-	$hash = $row['Hash'];
+    $hash = $row['Hash'];
 } else {
-	/* Generate new hashes */
-	$hash = bin2hex(openssl_random_pseudo_bytes(32));
-	
-	/* Add new hash for current user */
-	$query =	"INSERT INTO api_validate (Username, Hash, ValidUntil) " .
-			"    VALUES ('$username', '$hash', ADDTIME(NOW(), '1:00'))";
-	$res = & $db->query($query);
-	if (DB::isError($res))
-		die($res->getDebugInfo()); // Check for errors in query
+    /* Generate new hashes */
+    $hash = bin2hex(openssl_random_pseudo_bytes(32));
+
+    /* Add new hash for current user */
+    $query =    "INSERT INTO api_validate (Username, Hash, ValidUntil) " .
+            "    VALUES ('$username', '$hash', ADDTIME(NOW(), '1:00'))";
+    $res = & $db->query($query);
+    if (DB::isError($res))
+        die($res->getDebugInfo()); // Check for errors in query
 }
 
 echo "<p><a href='https://telegram.me/lesworkbot?start=$hash'>Connect to Telegram</a></p>\n";
