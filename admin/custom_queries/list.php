fabd8730d47014192dbd4fc8e758750b53f6ae77
Jonathan Dieter <jdieter@lesbg.com>
2016-12-12 13:54:05 +0200

    Convert dates to user date format using ugly regex.  When we switch to a better DB provider, we'll use the field type rather than a regex
    Signed-off-by: Jonathan Dieter <jdieter@lesbg.com>

--- admin/custom_queries/list.php
+++ admin/custom_queries/list.php
@@ -483,6 +483,9 @@ while ($row = & $res->fetchRow(DB_FETCHMODE_ASSOC)) {
         }
         $html .= "         <tr$alt>\n";
         foreach($row as $value) {
+            if(preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $value)) {
+                $value = date($dateformat, strtotime($value));
+            }
             $value = htmlspecialchars($value, ENT_QUOTES);
             $html .= "           <td>$value</td>\n";
         }
