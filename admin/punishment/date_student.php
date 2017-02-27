0da6d22306ce0a31e0345ff0891ac2f159647031
Jonathan Dieter <jdieter@lesbg.com>
2017-02-27 14:00:34 +0200

    Refactor code
    Signed-off-by: Jonathan Dieter <jdieter@lesbg.com>

--- admin/punishment/date_student.php
+++ admin/punishment/date_student.php
@@ -58,7 +58,8 @@ if ($row = & $res->fetchRow(DB_FETCHMODE_ASSOC)) {
 }
 
 $query = "SELECT DisciplineDateIndex, PunishDate, EndDate FROM disciplinedate " .
-         "WHERE DisciplineTypeIndex = $dtype " . "AND   Done = 0";
+         "WHERE DisciplineTypeIndex = $dtype " .
+         "AND   Done = 0";
 $res = &  $db->query($query);
 if (DB::isError($res))
     die($res->getDebugInfo()); // Check for errors in query
@@ -79,154 +80,8 @@ if ($row = & $res->fetchRow(DB_FETCHMODE_ASSOC)) {
 
 $title = "Students to be punished during next $disc";
 /* Make sure user has permission to view student's marks for subject */
-if (dbfuncGetPermission($permissions, $PERM_ADMIN) or
-     ($perm > $PUN_PERM_ALL and $is_teacher)) {
-    if ($_POST["action"] == "Check all") {
-        $check_all = 1;
-    } elseif ($_POST["action"] == "Uncheck all") {
-        $check_all = - 1;
-    } else {
-        $check_all = 0;
-    }
-    include "header.php";
-
-    $link = "index.php?location=" .
-             dbfuncString2Int("admin/punishment/date_student_action.php") .
-             "&amp;type=" . $_GET['type'] . "&amp;next=" . $_GET['next'];
-    $query = "SELECT user.Username, user.FirstName, user.Surname, discipline.Date, discipline.Comment, " .
-             "       class.ClassName, discipline.DisciplineIndex, disciplinedate.PunishDate " .
-             "       FROM user INNER JOIN discipline USING (Username) " .
-             "                 INNER JOIN disciplineweight USING (DisciplineWeightIndex) " .
-             "                 INNER JOIN classlist ON (user.Username=classlist.Username) " .
-             "                 INNER JOIN classterm USING (ClassTermIndex) " .
-             "                 INNER JOIN class USING (ClassIndex) " .
-             "                 LEFT OUTER JOIN disciplinedate USING (DisciplineDateIndex) " .
-             "                 LEFT OUTER JOIN (attendance INNER JOIN period USING (PeriodIndex)) " .
-             "                 ON ( " .
-             "                  discipline.Username = attendance.Username " .
-             "                  AND attendance.Date = '$pundate' " .
-             "                  AND period.Period = 1 " .
-             "                 ) " .
-             "                 LEFT OUTER JOIN attendancetype USING (AttendanceTypeIndex) " .
-             "WHERE  disciplineweight.YearIndex = $yearindex " .
-             "AND    (disciplinedate.PunishDate IS NULL AND discipline.Date <= '$enddate') " .
-             "AND    disciplineweight.DisciplineTypeIndex = $dtype " .
-             "AND    classterm.TermIndex = $termindex " .
-             "AND    class.YearIndex = $yearindex " .
-             "AND    class.DepartmentIndex = $depindex " .
-             "GROUP BY user.Username " .
-             "ORDER BY class.Grade, class.ClassName, discipline.Username ";
-    $res = &  $db->query($query);
-    if (DB::isError($res))
-        die($res->getDebugInfo()); // Check for errors in query
-
-    if ($res->numRows() > 0) {
-        /* Print punishments */
-
-        echo "      <form action='$link' method='post' name='pundate'>\n"; // Form method
-
-        echo "      <p align='center'>\n";
-        echo "         <input type='submit' name='action' value='Edit'>&nbsp; \n";
-        echo "         <input type='submit' name='action' value='Check all'>&nbsp; \n";
-        echo "         <input type='submit' name='action' value='Uncheck all'>&nbsp; \n";
-        echo "         <input type='submit' name='action' value='Done'> \n";
-        echo "      </p>\n";
-        echo "      <table align='center' border='1'>\n"; // Table headers
-        echo "         <tr>\n";
-        echo "            <th>&nbsp;</th>\n";
-        echo "            <th>Student</th>\n";
-        echo "            <th>Class</th>\n";
-        echo "            <th>Teacher</th>\n";
-        echo "            <th>Violation Date</th>\n";
-        echo "            <th>Reason</th>\n";
-        echo "         </tr>\n";
-
-        /* For each assignment, print a row with the title, date, score and comment */
-        $alt_count = 0;
-        while ( $row = & $res->fetchRow(DB_FETCHMODE_ASSOC) ) {
-            $alt_count += 1;
-            if ($alt_count % 2 == 0) {
-                $alt_step = "alt";
-            } else {
-                $alt_step = "std";
-            }
-            if ($check_all == 0) {
-                if (isset($_POST['mass'][$row['Username']])) {
-                    if ($_POST['mass'][$row['Username']] == "on") {
-                        $checked = "checked";
-                    } else {
-                        $checked = "";
-                    }
-                } else {
-                    if (! is_null($row['Done'])) {
-                        $checked = "checked";
-                    } else {
-                        $checked = "";
-                    }
-                }
-            } elseif ($check_all == 1) {
-                $checked = "checked";
-            } else {
-                $checked = "";
-            }
-            $alt = " class='$alt_step'";
-            echo "         <tr$alt>\n";
-            echo "            <td><input type='checkbox' name='mass[]' value='{$row['Username']}' id='check{$row['Username']}' $checked></input></td>\n";
-            echo "            <td><label for='check{$row['Username']}'>{$row['FirstName']} {$row['Surname']} ({$row['Username']})</label></td>\n";
-            echo "            <td><label for='check{$row['Username']}'>{$row['ClassName']}</label></td>\n";
-            $query = "SELECT Date, Comment, DisciplineIndex, tFirstName, tTitle, " .
-                     "       tSurname, PunishDate " .
-                     "       FROM view_discipline " .
-                     "WHERE  YearIndex = $yearindex " .
-                     "AND    tUsername IS NOT NULL " .
-                     "AND    ((PunishDate IS NULL AND Date <= '$enddate') " .
-                     "        OR Done = 0) " .
-                     "AND    DisciplineTypeIndex = $dtype " .
-                     "AND    Username = '{$row['Username']}' " .
-                     "ORDER BY Date, DisciplineIndex";
-            $nres = &  $db->query($query);
-            if (DB::isError($nres))
-                die($nres->getDebugInfo()); // Check for errors in query
-            echo "            <td nowrap><label for='check{$row['Username']}'>";
-            if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
-                echo "{$nrow['tTitle']} {$nrow['tFirstName']} {$nrow['tSurname']}";
-                while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
-                    echo "<br>{$nrow['tTitle']} {$nrow['tFirstName']} {$nrow['tSurname']}";
-                }
-            }
-            echo "</label></td>\n";
-            $nres = &  $db->query($query);
-            if (DB::isError($nres))
-                die($nres->getDebugInfo()); // Check for errors in query
-            echo "            <td nowrap><label for='check{$row['Username']}'>";
-            if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
-                $dateinfo = date($dateformat, strtotime($nrow['Date']));
-                echo "$dateinfo";
-                while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
-                    $dateinfo = date($dateformat, strtotime($nrow['Date']));
-                    echo "<br>$dateinfo";
-                }
-            }
-            echo "</label></td>\n";
-            $nres = &  $db->query($query);
-            if (DB::isError($nres))
-                die($nres->getDebugInfo()); // Check for errors in query
-            echo "            <td nowrap><label for='check{$row['Username']}'>";
-            if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
-                echo "{$nrow['Comment']}";
-                while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
-                    echo "<br>{$nrow['Comment']}";
-                }
-            }
-            echo "</label></td>\n";
-            echo "         </tr>\n";
-        }
-        echo "      </table>\n";
-        echo "      </form>\n";
-    } else {
-        echo "      <p align='center' class='subtitle'>No punishments of this type have been issued and not punished yet up to {$_POST['date']}.</p>\n";
-    }
-} else {
+if (!dbfuncGetPermission($permissions, $PERM_ADMIN) and
+     ($perm < $PUN_PERM_ALL or !$is_teacher)) {
     include "header.php";
 
     /* Log unauthorized access attempt */
@@ -235,7 +90,154 @@ if (dbfuncGetPermission($permissions, $PERM_ADMIN) or
 
     echo "      <p>You do not have permission to access this page</p>\n";
     echo "      <p><a href='$backLink'>Click here to go back</a></p>\n";
+    exit(0);
 }
+
+if ($_POST["action"] == "Check all") {
+    $check_all = 1;
+} elseif ($_POST["action"] == "Uncheck all") {
+    $check_all = - 1;
+} else {
+    $check_all = 0;
+}
+include "header.php";
+
+$link = "index.php?location=" .
+         dbfuncString2Int("admin/punishment/date_student_action.php") .
+         "&amp;type=" . $_GET['type'] . "&amp;next=" . $_GET['next'];
+$query = "SELECT user.Username, user.FirstName, user.Surname, discipline.Date, discipline.Comment, " .
+         "       class.ClassName, discipline.DisciplineIndex, disciplinedate.PunishDate " .
+         "       FROM user INNER JOIN discipline USING (Username) " .
+         "                 INNER JOIN disciplineweight USING (DisciplineWeightIndex) " .
+         "                 INNER JOIN classlist ON (user.Username=classlist.Username) " .
+         "                 INNER JOIN classterm USING (ClassTermIndex) " .
+         "                 INNER JOIN class USING (ClassIndex) " .
+         "                 LEFT OUTER JOIN disciplinedate USING (DisciplineDateIndex) " .
+         "                 LEFT OUTER JOIN (attendance INNER JOIN period USING (PeriodIndex)) " .
+         "                 ON ( " .
+         "                  discipline.Username = attendance.Username " .
+         "                  AND attendance.Date = '$pundate' " .
+         "                  AND period.Period = 1 " .
+         "                 ) " .
+         "                 LEFT OUTER JOIN attendancetype USING (AttendanceTypeIndex) " .
+         "WHERE  disciplineweight.YearIndex = $yearindex " .
+         "AND    (disciplinedate.PunishDate IS NULL AND discipline.Date <= '$enddate') " .
+         "AND    disciplineweight.DisciplineTypeIndex = $dtype " .
+         "AND    classterm.TermIndex = $termindex " .
+         "AND    class.YearIndex = $yearindex " .
+         "AND    class.DepartmentIndex = $depindex " .
+         "GROUP BY user.Username " .
+         "ORDER BY class.Grade, class.ClassName, discipline.Username ";
+$res = &  $db->query($query);
+if (DB::isError($res))
+    die($res->getDebugInfo()); // Check for errors in query
+
+if ($res->numRows() == 0) {
+    echo "      <p align='center' class='subtitle'>No punishments of this type have been issued and not punished yet up to {$_POST['date']}.</p>\n";
+    exit(0);
+}
+
+/* Print punishments */
+echo "      <form action='$link' method='post' name='pundate'>\n"; // Form method
+
+echo "      <p align='center'>\n";
+echo "         <input type='submit' name='action' value='Edit'>&nbsp; \n";
+echo "         <input type='submit' name='action' value='Check all'>&nbsp; \n";
+echo "         <input type='submit' name='action' value='Uncheck all'>&nbsp; \n";
+echo "         <input type='submit' name='action' value='Done'> \n";
+echo "      </p>\n";
+echo "      <table align='center' border='1'>\n"; // Table headers
+echo "         <tr>\n";
+echo "            <th>&nbsp;</th>\n";
+echo "            <th>Student</th>\n";
+echo "            <th>Class</th>\n";
+echo "            <th>Teacher</th>\n";
+echo "            <th>Violation Date</th>\n";
+echo "            <th>Reason</th>\n";
+echo "         </tr>\n";
+
+/* For each assignment, print a row with the title, date, score and comment */
+$alt_count = 0;
+while ( $row = & $res->fetchRow(DB_FETCHMODE_ASSOC) ) {
+    $alt_count += 1;
+    if ($alt_count % 2 == 0) {
+        $alt_step = "alt";
+    } else {
+        $alt_step = "std";
+    }
+    if ($check_all == 0) {
+        if (isset($_POST['mass'][$row['Username']])) {
+            if ($_POST['mass'][$row['Username']] == "on") {
+                $checked = "checked";
+            } else {
+                $checked = "";
+            }
+        } else {
+            if (! is_null($row['Done'])) {
+                $checked = "checked";
+            } else {
+                $checked = "";
+            }
+        }
+    } elseif ($check_all == 1) {
+        $checked = "checked";
+    } else {
+        $checked = "";
+    }
+    $alt = " class='$alt_step'";
+    echo "         <tr$alt>\n";
+    echo "            <td><input type='checkbox' name='mass[]' value='{$row['Username']}' id='check{$row['Username']}' $checked></input></td>\n";
+    echo "            <td><label for='check{$row['Username']}'>{$row['FirstName']} {$row['Surname']} ({$row['Username']})</label></td>\n";
+    echo "            <td><label for='check{$row['Username']}'>{$row['ClassName']}</label></td>\n";
+    $query = "SELECT Date, Comment, DisciplineIndex, tFirstName, tTitle, " .
+             "       tSurname, PunishDate " .
+             "       FROM view_discipline " .
+             "WHERE  YearIndex = $yearindex " .
+             "AND    tUsername IS NOT NULL " .
+             "AND    ((PunishDate IS NULL AND Date <= '$enddate') " .
+             "        OR Done = 0) " .
+             "AND    DisciplineTypeIndex = $dtype " .
+             "AND    Username = '{$row['Username']}' " .
+             "ORDER BY Date, DisciplineIndex";
+    $nres = &  $db->query($query);
+    if (DB::isError($nres))
+        die($nres->getDebugInfo()); // Check for errors in query
+    echo "            <td nowrap><label for='check{$row['Username']}'>";
+    if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
+        echo "{$nrow['tTitle']} {$nrow['tFirstName']} {$nrow['tSurname']}";
+        while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
+            echo "<br>{$nrow['tTitle']} {$nrow['tFirstName']} {$nrow['tSurname']}";
+        }
+    }
+    echo "</label></td>\n";
+    $nres = &  $db->query($query);
+    if (DB::isError($nres))
+        die($nres->getDebugInfo()); // Check for errors in query
+    echo "            <td nowrap><label for='check{$row['Username']}'>";
+    if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
+        $dateinfo = date($dateformat, strtotime($nrow['Date']));
+        echo "$dateinfo";
+        while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
+            $dateinfo = date($dateformat, strtotime($nrow['Date']));
+            echo "<br>$dateinfo";
+        }
+    }
+    echo "</label></td>\n";
+    $nres = &  $db->query($query);
+    if (DB::isError($nres))
+        die($nres->getDebugInfo()); // Check for errors in query
+    echo "            <td nowrap><label for='check{$row['Username']}'>";
+    if ($nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC)) {
+        echo "{$nrow['Comment']}";
+        while ( $nrow = & $nres->fetchRow(DB_FETCHMODE_ASSOC) ) {
+            echo "<br>{$nrow['Comment']}";
+        }
+    }
+    echo "</label></td>\n";
+    echo "         </tr>\n";
+}
+echo "      </table>\n";
+echo "      </form>\n";
 
 include "footer.php";
 ?>
